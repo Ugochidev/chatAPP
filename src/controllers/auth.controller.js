5cdf8ea56a1d8a0103a5424a72778101882c239e
Ugochidev <ugochukwuchioma16@gmail.com>
2023-03-14 16:21:41 +0100

    modified

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -29,7 +29,7 @@ const createUser = async (req, res, next) => {
       lastName,
       userName,
       email,
-      password,
+      password: hashPassword,
     });
     newUser.set("password", undefined);
     return res.status(201).json({
